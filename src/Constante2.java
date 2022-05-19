0cb054c0649bb062a229b1fec943f6b6c4ee05e7
Jose Valenzuela <valenz74@gmail.com>
2022-05-19 08:09:56 -0500

    Aprendiendo sobre Constantes en Java

---
+++ src/Constante2.java
@@ -0,0 +1,10 @@
+public class Constante2 {
+    public static void main(String[] args) {
+        final int Constante2 = 9;
+        int a = 15;
+        int b = 21;
+//        Constante2 = a - b;
+        System.out.println(Constante2);
+
+    }
+}
